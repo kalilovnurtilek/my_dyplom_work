80c3fca7b9ecdf10f26f4816fad9929fd131ad80
Kalilov Nurtilek <nurtilekdeveloper@gmail.com>
2025-03-12 08:11:47 +0600

    second

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,8 @@
+from django.urls import path
 
+from posts.views import hello, get_index
 
 urlpatterns = [
-
+path("hello/", hello, name="helo-view"),
+path("", get_index, name="index-page"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hello(request):
+    body = "<h1>Hello</h1>"
+    # body = """
+#     <!DOCTYPE html>
+# <html>
+#     <head>
+#         <title>Geek TEST</title>
+#     </head>
+# <body>
+
+#         <h1>Загаловок первого уровня</h1>
+#         <p>Параграф</p>
+
+# </body>
+# </html>
+#     """
+
+    headers = {"name": "Alex",}
+            #    "Content-Type" :"application/vnd.ms-exel"}
+    return HttpResponse(body, headers=headers, status=500)
+
+def get_index(request):
+    print(request.user)
+    if request.method=="GET":
+        return HttpResponse("Главная страница")
+    else:
+        return HttpResponse("Не тот метод запроса")
